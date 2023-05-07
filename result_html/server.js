03b40af3182b2a08056a95ca77e95ce0f32e3bcc
❄snow_____field❄ <86102527+asnowfield@users.noreply.github.com>
2023-05-07 21:43:40 +0900

    complete result_html

--- result_html/server.js
+++ result_html/server.js
@@ -1,8 +1,11 @@
+const sqlite3 = require("sqlite3").verbose();
+const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 8080;
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static("static"));
 
 app.get("/", (req, res) => {
@@ -26,12 +29,9 @@ app.get("/clkvalue/:dificult", (req, res) => {
   const { dificult } = req.params;
   if (dificult == "ez") {
     res.sendFile(__dirname + "/quiz.html");
-    // res.json({'dificulty': 'ez'})
   } else if (dificult == "nm") {
-    // res.json({'dificulty': 'nm'})
     res.sendFile(__dirname + "/quiz.html");
   } else {
-    // res.json({'dificulty':'hd'})
     res.sendFile(__dirname + "/quiz.html");
   }
 });
@@ -40,32 +40,34 @@ app.get("/result", (req, res) => {
   res.sendFile(__dirname + "/result.html");
 });
 
-app.get("/rank",(req,res)=>{
-  res.sendFile(__dirname+"/rank.html");
-})
+app.get("/rank", (req, res) => {
+  res.sendFile(__dirname + "/rank.html");
+});
+
+app.post("/resultData", (req, res) => {
+  var score = req.body.score;
+  var userName = req.body.userName;
+  // open the database
+  let db = new sqlite3.Database("quiz.db");
+
+  // insert data into the quizDB table
+  db.run(
+    `INSERT INTO quizDB (score, userName) VALUES (?, ?)`,
+    [score, userName],
+    function (err) {
+      if (err) {
+        return console.log(err.message);
+      }
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+    }
+  );
+
+  // close the database
+  db.close();
+
+  res.sendStatus(200);
+});
 
 app.listen(port, () => {
   console.log("serverOn");
 });
-
-// app.get('/user/:id', (req, res)=>{
-//   const q = req.params
-//   console.log(q.id)
-//   const q = req.query
-//   console.log(q.q)
-//   res.json({'userid':q.name})
-// });
-
-// app.get('/quiz/:clkValue',(req,res)=>{
-//   const {clkValue} = req.params
-//   if (clkValue == "ez") {
-//     res.json({'dificulty': 'ez'})
-//     res.sendFile(__dirname+'/quiz.html');
-//   } else if (clkValue == "nm") {
-//     res.json({'dificulty': 'nm'})
-//     res.sendFile(__dirname+'/quiz.html');
-//   } else {
-//     res.json({'dificulty':'hd'})
-//     res.sendFile(__dirname+'/quiz.html');
-//   }
-// })
